ab043f89d5989ebe2f952a8ad4e17dd723223213
Wenting04 <wentingzhang1928@gmail.com>
2026-05-27 12:50:54 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar el uso de HTTPS si la app está en producción (ej: en Railway)
+        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
+            URL::forceSCHEME('https');
+        }
     }
 }
