8dae8e8a87ea9efb4bb84a0df20f6a251a58a912
Wenting04 <wentingzhang1928@gmail.com>
2026-05-27 13:05:30 +0200

    Integrate Contentsquare for heatmap tracking
    Added Contentsquare script for heatmap tracking.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,9 @@
         <!-- Favicon: icono nombre en pestaña web -->
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
+        <!-- Contentsquare para Mapa de Calor en hotjar -->
+        <script src="https://t.contentsquare.net/uxa/86e7dac14aae7.js"></script>
+
         <link rel="stylesheet" href="{{ asset('css/index.css') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
